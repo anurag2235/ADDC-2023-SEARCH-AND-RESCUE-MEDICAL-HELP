a96f8d469d05d5741bab61702b088c90765fc8b4
Anurag Shekhar <114636450+anurag2235@users.noreply.github.com>
2023-04-28 15:07:02 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -21,6 +21,28 @@ try:
         if A == True:      
             detections = yolov7.detect(frame)
             detected_frame = draw(frame, detections)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(detected_frame, 
+                'aeroKLE', 
+                (30, 50), 
+                font, 1, 
+                (0, 0, 255), 
+                2, 
+                cv2.LINE_4)
+            cv2.putText(detected_frame, 
+                'SAE ADDC 2023', 
+                (30, 100), 
+                font, 1, 
+                (0, 0, 255), 
+                2, 
+                cv2.LINE_4)
+            cv2.putText(detected_frame, 
+                'NUMBER OF PEOPLE IN NEED', 
+                (150, 50), 
+                font, 1, 
+                (0, 255, 0), 
+                2, 
+                cv2.LINE_4)          
             if len(detections)!=0:
                 i=0
                 for items in detections:
@@ -29,6 +51,13 @@ try:
                     count=count+1
                    i=i+1
                 print("DETECTED PERSON = ",count)
+            cv2.putText(detected_frame, 
+                str(count), 
+                (500, 100), 
+                font, 1, 
+                (0, 255, 0), 
+                2, 
+                cv2.LINE_4)     
             cv2.imshow('webcam', detected_frame)
             cv2.waitKey(1)
         else:
