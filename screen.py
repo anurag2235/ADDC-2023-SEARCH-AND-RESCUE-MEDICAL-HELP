7e27f56632937f6e77455b5b101f60d354941d04
Anurag Shekhar <114636450+anurag2235@users.noreply.github.com>
2023-04-28 11:34:16 +0530

    Create screen.py

---
+++ screen.py
@@ -0,0 +1,40 @@
+from algorithm.object_detector import YOLOv7
+from utils.detections import draw
+import json
+import cv2
+import imutils
+import numpy as np
+from mss import mss
+from PIL import Image
+sct = mss()
+yolov7 = YOLOv7()
+yolov7.load('coco.weights', classes='coco.yaml', device='cpu') # use 'gpu' for CUDA GPU inference
+try:
+    while True:
+        w, h = 640, 640
+        monitor = {'top': 0, 'left': 0, 'width': w, 'height': h}
+        img = Image.frombytes('RGB', (w,h), sct.grab(monitor).rgb)
+        img_np = np.array(img)
+        frame = img_np
+        count=0
+        A=True
+        if A == True:      
+            detections = yolov7.detect(frame)
+            detected_frame = draw(frame, detections)
+            if len(detections)!=0:
+                i=0
+                for items in detections:
+                   identity=detections[i]['class']
+                   if identity=='person':
+                    count=count+1
+                   i=i+1
+                print("DETECTED PERSON = ",count)
+            cv2.imshow('webcam', detected_frame)
+            cv2.waitKey(1)
+        else:
+            break
+except KeyboardInterrupt:
+    pass
+webcam.release()
+print('[+] webcam closed')
+yolov7.unload()
